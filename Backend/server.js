951795a1c6524ebf88afc114c8320ee6a7718106
abdulhamid_al_mallah <abd49717@gmail.com>
2025-10-25 01:40:12 +0300

    finish setup-server

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv"
+import carRouter from "./routes/carRoutes.js"
+import errorController from "./controllers/errorController.js"
 
-// Basic route
-app.get('/', (req, res) => {
-  res.send('Server is running...');
-});
+dotenv.config()
 
-// Start server
+app.use("/api", carRouter)
+app.use(errorController)
+
+
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
