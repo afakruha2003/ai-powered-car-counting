cc5dcacf96a2f6947761697273914d4d0975f39b
abdulhamid_al_mallah <abd49717@gmail.com>
2025-10-29 04:36:45 +0300

    create the controller functions

--- Backend/controllers/carController.js
+++ Backend/controllers/carController.js
@@ -1,10 +1,37 @@
 import asyncHandler from "express-async-handler";
 import CustomError from "../utilities/customError.js";
+import Counter from "../models/counterModel.js";
 
-export const printMsg = asyncHandler(async (req, res) => {
-   if (1 == 1) throw new CustomError("this error from print function", 404);
+export const incrementCounter = asyncHandler(async (req, res) => {
+   const { count } = req.params;
+
+   if (!count) throw new CustomError("please provide the count !!", 400);
+
+   const updated = await Counter.findOneAndUpdate({ name: "carCounter" }, { $inc: { value: count } }, { new: true });
+
    res.status(200).json({
       success: true,
-      location: "from car controller",
+      message: "counter increment done successfully.",
+      count: updated.value,
+   });
+});
+
+export const resetCounter = asyncHandler(async (req, res) => {
+   const updated = await Counter.findOneAndUpdate({ name: "carCounter" }, { $set: { value: 0 } }, { new: true });
+
+   res.status(200).json({
+      success: true,
+      message: "counter resete done successfully.",
+      count: updated.value,
+   });
+});
+
+export const getCounter = asyncHandler(async (req, res) => {
+   const count = await Counter.findOne({ name: "carCounter" });
+
+   res.status(200).json({
+      success: true,
+      message: "counter resete done successfully.",
+      count,
    });
 });
